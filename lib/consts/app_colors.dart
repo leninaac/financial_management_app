3779ecc89d98af3e96a03cb47d693ccaa24c5472
leninaac <C@nut087>
2024-06-21 07:38:27 -0300

    Feature: Insert gradient colors

--- lib/consts/app_colors.dart
+++ lib/consts/app_colors.dart
@@ -10,4 +10,10 @@ class AppColors {
   static const Color graybase1 = Color(0xff252525);
   static const Color graybase2 = Color(0xff212121);
   static const Color graybase3 = Color(0xff79767D);
+
+//Gradient States
+  static const List<Color> gradientMint = [Color(0xffEAEAEA), Color(0xffB2D0CE)];
+  static const List<Color> gradientYellow = [Color(0xffFCFFDF), Color(0xffF1FE87)];
+  static const List<Color> gradientLilac = [Color(0xffF2EFF4), Color(0xffB8A9C6)];
+
 }
